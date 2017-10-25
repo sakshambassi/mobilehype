600098bdba3abee1a1b1dc4734db44b321080ffb
Saksham Bassi <sakshambassi@gmail.com>
2017-10-26 00:52:33 +0530

    changed mobile brand url

--- brandscrape.py
+++ brandscrape.py
@@ -3,21 +3,18 @@ import urllib
 import pandas
 from pandas import DataFrame
 
-url = "https://en.wikipedia.org/wiki/List_of_mobile_phone_makers_by_country"
+url = "https://www.gsmarena.com/makers.php3"
 htmlfile = urllib.request.urlopen(url)
 l = ['ID', 'Company Name']
 i=0
 df = pandas.DataFrame(columns=l)
 soup = bs4.BeautifulSoup(htmlfile, "html.parser")
-for company in soup.find_all('a'):
-	if company.get_text() == "":
-		continue
+div=soup.find('div',class_="st-text")
+for company in div.find_all('td'):
 	i = i + 1
-	if i < 4:
-		continue
-	df.loc[i,'ID'] = int(i-3)
-	df.loc[i,'Company Name'] = company.get_text()
-	if company.get_text() == "Vertu":
-		break
+	df.loc[i,'ID']=i
+	df.loc[i,'Company Name'] = company.find_next('a').contents[0]
+	# if company.get_text() == "Vertu":
+	# 	break
 
 df.to_excel('mobiles3.xlsx',index=False)
