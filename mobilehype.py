8e96ed17d53d76f77c93bb542c4cdca388a804e7
Saksham Bassi <sakshambassi@gmail.com>
2017-10-26 19:59:08 +0530

    added mobile names and searches

--- mobilehype.py
+++ mobilehype.py
@@ -1,5 +1,6 @@
 import tweepy
 import pandas
+import numpy as np
 import re
 from textblob import TextBlob
 
@@ -10,13 +11,15 @@ access_token = '1424811614-jMFO115CUxnBcwuNHtzZznZhkb3m52Q2rZVeVUm'
 access_token_secret = 'ZDChujaMv6eTZnDw0kKqL00fFD0DqKp9XSlp1p8jsJ4ZO'
 
 rd = pandas.read_excel('mobiles.xlsx')
+df = pandas.read_excel('hyperesult.xlsx')
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
 searches= ['upcoming smartphone','upcoming mobile','upcoming mobile phone','smartphone','mobile phone']
-companies = rd['Company Name'].values
+companies = np.unique(rd['Brand'].values)
+
 for search in searches:
 	print('\n\n')
 	public_tweets = api.search(search,count=100)
@@ -31,14 +34,17 @@ for search in searches:
 			for company in companies:
 				if word == company:
 					print(word, company)
-					rd.loc[i, 'Hype Value'] += (1+analysis.sentiment.polarity)*0.01
+					df.loc[i, 'Hype Value'] += (1+analysis.sentiment.polarity)*0.01
 				i=i+1
 
+hypes = df['Hype Value'].values
 i=0
-hypes = rd['Hype Value'].values
 for hype in hypes:
-	hype = (hype/sum(hypes))*100
-	rd.loc[i,'Hype Percent'] = hype
+	if sum(hypes)==0:
+		hype = 0
+	else:
+		hype = (hype/sum(hypes))*100
+	df.loc[i,'Hype Percent'] = hype
 	i=i+1
 
-rd.to_excel('mobiles.xlsx',index=False)
+df.to_excel('hyperesult.xlsx',index=False)
